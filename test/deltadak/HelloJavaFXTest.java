dee8723a10ce88c485054758fab0f0d0a18f4cf8
Thomas <hollandpirates@gmail.com>
2018-02-27 18:20:03 +0100

    use built-in JUnit 5

--- test/deltadak/HelloJavaFXTest.java
+++ test/deltadak/HelloJavaFXTest.java
@@ -1,22 +1,22 @@
-package deltadak;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- * Tests {@link HelloJavaFX} class.
- */
-class HelloJavaFXTest {
-
-    /**
-     * Test {@link HelloJavaFX#calculate()} method.
-     */
-    @Test
-    public void testCalculate(){
-        HelloJavaFX helloJavaFX = new HelloJavaFX();
-
-        assertEquals(2, helloJavaFX.calculate(), "message");
-    }
-
-}
+//package deltadak;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+///**
+// * Tests {@link HelloJavaFX} class.
+// */
+//class HelloJavaFXTest {
+//
+//    /**
+//     * Test {@link HelloJavaFX#calculate()} method.
+//     */
+//    @Test
+//    public void testCalculate(){
+//        HelloJavaFX helloJavaFX = new HelloJavaFX();
+//
+//        assertEquals(2, helloJavaFX.calculate(), "message");
+//    }
+//
+//}
