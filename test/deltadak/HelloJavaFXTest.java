75ca94064e52dc71ebefb69478882327de7d200f
Thomas <hollandpirates@gmail.com>
2019-02-10 14:30:31 +0100

    Clean up tests

--- test/deltadak/HelloJavaFXTest.java
+++ test/deltadak/HelloJavaFXTest.java
@@ -13,28 +13,10 @@ class HelloJavaFXTest {
      * Test {@link HelloJavaFX#calculate()} method.
      */
     @Test
-    public void testCalculate(){
+    public void testCalculate() {
         HelloJavaFX helloJavaFX = new HelloJavaFX();
 
         assertEquals(2, helloJavaFX.calculate(), "message");
     }
-
-//    /**
-//     * Fails on `new ProgressIndicator();` line.
-//     */
-//    @Test
-//    public void testProgress() {
-//        HelloJavaFX helloJavaFX = new HelloJavaFX();
-//
-//        // Initialise JavaFX Toolkit, needed for using elements like ProgressIndicator.
-//        com.sun.javafx.application.PlatformImpl.startup(()->{});
-//
-//        ProgressIndicator progressIndicator = new ProgressIndicator();
-//
-//        assertTrue(helloJavaFX.isLoading(progressIndicator));
-//
-//        // Shutdown toolkit.
-//        com.sun.javafx.application.PlatformImpl.exit();
-//    }
 
 }
--- test/deltadak/HelloWorldTest.java
+++ test/deltadak/HelloWorldTest.java
@@ -1,8 +1,8 @@
 package deltadak;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Test {@link HelloWorld} class.
@@ -12,7 +12,7 @@ public class HelloWorldTest {
      * Test {@link HelloWorld#calculate()} method.
      */
     @Test
-    public void testHelloWorld(){
+    public void testHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals(2, helloWorld.calculate(), "message");
     }
